98bfcebbf46263c532879b099407ea9a74be0f9f
alex-chulu <chulualexandar@gmail.com>
2023-07-15 06:11:13 +0000

    task 0 fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
 """
-Calculates the fewest number of operations needed to obtain exactly n 'H' characters in a file,
+Calculates the fewest number of operations 
+needed to obtain exactly n 'H' characters in a file,
 using the Copy All and Paste operations.
 """
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to obtain exactly n 'H' characters in a file.
+    Calculates the fewest number of operations needed 
+    to obtain exactly n 'H' characters in a file.
 
     Args:
         n (int): The desired number of 'H' characters.
 
     Returns:
-        int: The fewest number of operations required. Returns 0 if n is impossible to achieve.
+        int: The fewest number of operations required. 
+        Returns 0 if n is impossible to achieve.
     """
     if n == 0:
         return 0
