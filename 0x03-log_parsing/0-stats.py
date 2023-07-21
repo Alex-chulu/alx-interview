3a63144702e3bb98f4ba7f534bfa5fb2ff1741f7
alex-chulu <chulualexandar@gmail.com>
2023-07-21 00:41:12 +0000

    task 0 fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,9 +7,8 @@ Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code>
 After every 10 lines and/or a keyboard interruption (CTRL + C), print statistics.
 """
 
-import sys
 
-def print_statistics(total_file_size, status_counts):
+def print_statistics(file_size, codes):
     """
     Print the computed statistics.
 
@@ -20,43 +19,45 @@ def print_statistics(total_file_size, status_counts):
     Returns:
         None
     """
-    print("Total file size: File size: {}".format(total_file_size))
-    for status_code in sorted(status_counts.keys()):
-        print("{}: {}".format(status_code, status_counts[status_code]))
-
-def main():
-    total_file_size = 0
-    status_counts = {}
-
-    try:
-        line_number = 0
-        for line in sys.stdin:
-            line_number += 1
-            # Parse the line using the provided input format
-            parts = line.split()
-            if len(parts) != 9:
-                # Skip lines that don't match the format
-                continue
-
-            status_code = parts[-3]
-            try:
-                file_size = int(parts[-1])
-            except ValueError:
-                # Skip lines where the file size is not an integer
-                continue
-
-            # Update the total file size and status code count
-            total_file_size += file_size
-            status_counts[status_code] = status_counts.get(status_code, 0) + 1
-
-            if line_number % 10 == 0:
-                # Print statistics after every 10 lines
-                print_statistics(total_file_size, status_counts)
-
-    except KeyboardInterrupt:
-        # If a keyboard interruption (CTRL + C) occurs, print the current statistics
-        print_statistics(total_file_size, status_counts)
+    print("Total file size: File size: {}".format(file_size))
+    for key in sorted(codes):
+        print("{}: {}".format(key, codes[key]))
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    file_size = 0
+    codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(file_size, codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                file_size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if codes.get(line[-2], -1) == -1:
+                        [line[-2]] = 1
+                    else:
+                        codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(file_size, codes)
+
+    except KeyboardInterrupt:
+        print_stats(file_size, codes)
+        raise
 
