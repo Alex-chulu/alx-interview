d6ee77b4a7c9525dafb8e241f38db975526c2cae
alex-chulu <chulualexandar@gmail.com>
2023-07-23 13:09:30 -0700

    task 0 fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,62 +6,55 @@ Description: Reads stdin line by line and computes metrics.
 Input format: <IP Address> - [<date>] "GET /projects/260 
 HTTP/1.1" <status code> <file size>
 After every 10 lines and/or a keyboard interruption 
-(CTRL + C), print statistics.
+(CTRL + C), print statistics
 """
+
 
 import sys
 
-def print_statistics(file_size, codes):
-    """
-    Print the computed statistics.
 
-    Args:
+if __name__ == '__main__':
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+
+    def print_statistics():
+        """
+        Print the computed statistics.
+
+        Args:
         total_file_size (int): The total file size accumulated so far.
         status_counts (dict): A dictionary containing counts 
         of each status code.
 
-    Returns:
-        None
-    """
-    print("Total file size: File size: {}".format(file_size))
-    for key in sorted(codes):
-        print("{}: {}".format(key, codes[key]))
+        Returns:
+            None
+        """
+        print('Total file size: {}'.format(size[0]))
+        for key in sorted(codes.keys()):
+            if codes[key]:
+                print('{}: {}'.format(key, codes[key]))
 
-if __name__ == "__main__":
-    import sys
+    def parse_line(line):
+        """ Checks the line for an object """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            size[0] += int(word[-1])
+            code = int(word[-2])
+            if code in codes:
+                codes[code] += 1
+        except BaseException:
+            pass
 
-    file_size = 0
-    codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
+    num = 1
     try:
         for line in sys.stdin:
-            if count == 10:
-                print_stats(file_size, codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                file_size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if codes.get(line[-2], -1) == -1:
-                        [line[-2]] = 1
-                    else:
-                        codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(file_size, codes)
-
+            parse_line(line)
+            if num % 10 == 0:
+                print_statistics()
+            num += 1
     except KeyboardInterrupt:
-        print_stats(file_size, codes)
+        print_statistics()
         raise
-
+    print_statistics()
