bb8c680e54092c8fba9ee7895591b7b4885da812
alex-chulu <chulualexandar@gmail.com>
2023-09-05 21:01:55 +0000

    task 0 fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,32 +1,10 @@
 #!/usr/bin/python3
-"""
-isWinner - Determine the winner of a prime number game.
-"""
 
 def isWinner(x, nums):
     if not x or not nums:
         return None
-    """
-    Determine the winner of the prime number game.
 
-    Args:
-    - x: The number of rounds to be played.
-    - nums: An array of integers representing the range of numbers for each round.
-
-    Returns:
-    - The name of the player that won the most rounds.
-    - If the winner cannot be determined, return None.
-    """
     def is_prime(n):
-        """
-        Check if a number is prime.
-
-        Args:
-        - n: The number to check for primality.
-
-        Returns:
-        - True if the number is prime, False otherwise.
-        """
         if n <= 1:
             return False
         if n <= 3:
@@ -39,15 +17,6 @@ def isWinner(x, nums):
                 return False
             i += 6
         return True
-        """
-        Find the winner of a single round.
-
-        Args:
-        - n: The upper limit of the number range for this round.
-
-        Returns:
-        - The name of the player that wins this round.
-        """
 
     def calculate_winner(n):
         primes = [i for i in range(2, n + 1) if is_prime(i)]
