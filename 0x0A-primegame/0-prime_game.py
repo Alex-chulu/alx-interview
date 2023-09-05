7a0cd5450e35c905eec6366f1df1d0077414e254
alex-chulu <chulualexandar@gmail.com>
2023-09-05 21:04:04 +0000

    task 0 fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,41 +1,77 @@
 #!/usr/bin/python3
+"""
+isWinner - Determine the winner of a prime number game.
+"""
 
 def isWinner(x, nums):
-    if not x or not nums:
-        return None
+    """
+    Determine the winner of the prime number game.
 
+    Args:
+    - x: The number of rounds to be played.
+    - nums: An array of integers representing the range of numbers for each round.
+
+    Returns:
+    - The name of the player that won the most rounds.
+    - If the winner cannot be determined, return None.
+    """
     def is_prime(n):
-        if n <= 1:
+        """
+        Check if a number is prime.
+
+        Args:
+        - n: The number to check for primality.
+
+        Returns:
+        - True if the number is prime, False otherwise.
+        """
+        if n < 2:
             return False
-        if n <= 3:
-            return True
-        if n % 2 == 0 or n % 3 == 0:
-            return False
-        i = 5
-        while i * i <= n:
-            if n % i == 0 or n % (i + 2) == 0:
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
                 return False
-            i += 6
         return True
 
-    def calculate_winner(n):
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        if len(primes) % 2 == 0:
-            return "Ben"
-        return "Maria"
+    def find_winner(n):
+        """
+        Find the winner of a single round.
 
-    winners = [calculate_winner(n) for n in nums]
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
+        Args:
+        - n: The upper limit of the number range for this round.
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    return None
+        Returns:
+        - The name of the player that wins this round.
+        """
+        maria_turn = True  # Maria goes first
 
-if __name__ == "__main__":
-    x = 3
-    nums = [4, 5, 1]
-    print("Winner: {}".format(isWinner(x, nums)))
+        while n >= 2:
+            prime_found = False
+
+            # Find the largest prime number in the range [2, n]
+            for i in range(n, 1, -1):
+                if is_prime(i):
+                    prime_found = True
+                    n -= i  # Remove the prime number and its multiples
+                    break
+
+            if not prime_found:
+                break
+
+            maria_turn = not maria_turn  # Switch turns
+
+        return "Maria" if maria_turn else "Ben"
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        winner = find_winner(n)
+        if winner == "Maria":
+            maria_wins += 1
+        elif winner == "Ben":
+            ben_wins += 1
+
+    if maria_wins == ben_wins:
+        return None
+    return "Maria" if maria_wins > ben_wins else "Ben"
 
