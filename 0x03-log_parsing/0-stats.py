dcf8094fd11e148ab680e45dd6154b379d0fc18d
alex-chulu <chulualexandar@gmail.com>
2023-09-10 21:35:45 +0000

    task 0 fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,6 +3,13 @@
 import sys
 
 def print_statistics(total_size, status_codes):
+    """
+    Prints the total file size and the count of each status code.
+
+    Args:
+        total_size (int): The total file size.
+        status_codes (dict): A dictionary containing status codes as keys and their counts as values.
+    """
     print("File size: {}".format(total_size))
     sorted_status_codes = sorted(status_codes.items())
     for code, count in sorted_status_codes:
@@ -10,6 +17,11 @@ def print_statistics(total_size, status_codes):
             print("{}: {}".format(code, count))
 
 def main():
+    """
+    Main function to read and process log lines from stdin.
+
+    Reads log lines from stdin, extracts information such as status code and file size, and calculates statistics.
+    """
     total_size = 0
     status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
